bd47727ac3d086f687a8330235bbc2666d2fce0c
Klete Rittger <klete@vestmed.com>
2023-05-28 14:28:44 -0500

    updated main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
-// import router from './router'
-// import { GStore } from './store'
+import router from './router'
+import { GStore } from './store'
 
-// import Modal from './components/Modal.vue'
+import Modal from './components/Modal.vue'
 
 import './assets/css/reset.css'
 import './assets/css/color-themes.css'
@@ -12,31 +12,26 @@ import './assets/css/base.css'
 import './assets/css/form.css'
 import './assets/css/print.css'
 
-// import { VueFire } from 'vuefire'
-// import firebaseApp from '@/firebase/config.js'
-// import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { VueFire } from 'vuefire'
+import firebaseApp from '@/firebase/config.js'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
-// const auth = getAuth(firebaseApp)
+const auth = getAuth(firebaseApp)
 
 let app
 
-app = createApp(App)
-// app.use(router)
-// app.component('modal', Modal)
-app.mount('#app')
-
 console.log(import.meta.env)
 
-// onAuthStateChanged(auth, () => {
-//   if (!app) {
-//     app = createApp(App)
-//     app.use(router)
-//     app.use(VueFire, {
-//       firebaseApp,
-//       modules: [],
-//     })
-//     app.component('modal', Modal)
-//     app.provide('GStore', GStore)
-//     app.mount('#app')
-//   }
-// })
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App)
+    app.use(router)
+    app.use(VueFire, {
+      firebaseApp,
+      modules: [],
+    })
+    app.component('modal', Modal)
+    app.provide('GStore', GStore)
+    app.mount('#app')
+  }
+})
